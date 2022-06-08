3a2dbb755e0e698913437d36f659f2048621f8c3
abraham <apolichenco@gmail.com>
2022-06-08 15:02:43 -0400

    commit tree 43c137ccad5f588464c9b38287e23bbc79071c07

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    // const matchingDrivers = []
+    // for (const user of drivers) {
+    //     if (user.toLowerCase() === string.toLowerCase()) {
+    //         matchingDrivers.push(user)    
+    //     }
+    // }
+    // return matchingDrivers
+    return drivers.filter(user => user.toLowerCase() === string.toLowerCase())
+     
+}
+
+function fuzzyMatch(drivers, string){
+    // const almosMatchingDrivers = []
+    // for (const user of drivers) {
+    //     if (user.startsWith(string)) {
+    //         almosMatchingDrivers.push(user)
+    //     }
+    // }
+    // return almosMatchingDrivers
+    return drivers.filter(user => user.startsWith(string))
+}
+
+function matchName(drivers, string){
+    return drivers.filter(user => user.name === string)
+}
